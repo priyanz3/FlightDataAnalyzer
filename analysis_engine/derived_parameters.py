39b5859d68eaf2dc8a5d8ca99de1ad50384d01ff
Robert Parker <robert.parker@flightdataservices.com>
2015-12-09 09:53:15 +0000

    Helicopter: Altitude AAL fix

--- analysis_engine/derived_parameters.py
+++ analysis_engine/derived_parameters.py
@@ -835,7 +835,8 @@ class AltitudeAAL(DerivedParameterNode):
             plt.plot(alt_rad.array, 'r-')
             plt.show()
         '''
-        alt_aal = np.ma.maximum(alt_aal, alt_rad.array.data)
+        if alt_rad:
+            alt_aal = np.ma.maximum(alt_aal, alt_rad.array.data)
         self.array = alt_aal
 
 
