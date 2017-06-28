492bd18d28dc679d168eb4409fc95badb358333b
Dave Jesse <dave.jesse@flightdataservices.com>
2017-06-28 12:10:17 +0100

    The useful parameter of the descent in feet per nautical mile at a 3 deg approach path is provided. Commonly known by aircrew as "300ft per mile".

--- analysis_engine/settings.py
+++ analysis_engine/settings.py
@@ -235,6 +235,12 @@ CONTROL_COLUMN_IN_USE_RATIO = 1.30  # %
 # approaches, go-around and touch-and-go phases and instances derive.
 DESCENT_LOW_CLIMB_THRESHOLD = 500 #ft
 
+# This strange conversion is for the tail clearance calculation
+FEET_PER_NM = 6076
+
+# This is the ratio of distances for a 3 deg approach
+FEET_PER_NM_3_DEG = 318.435
+
 # Acceleration due to gravity
 GRAVITY_IMPERIAL = 32.2  # ft/sec^2 - used for combining acceleration and height terms
 
